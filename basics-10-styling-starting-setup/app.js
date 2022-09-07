dc4142af26b8fbaaf428e82fac6907142af911b0
Long Thay <long.tb@ht-tech.vn>
2022-09-07 10:51:57 +0700

    Add dynamic style and use computed properties

--- basics-10-styling-starting-setup/app.js
+++ basics-10-styling-starting-setup/app.js
@@ -6,6 +6,17 @@ const app = Vue.createApp({
       boxCSelected: false,
     };
   },
+  computed: {
+    boxAClasses() {
+      return { active: this.boxASelected };
+    },
+    boxBClasses() {
+      return { active: this.boxBSelected };
+    },
+    boxCClasses() {
+      return { active: this.boxCSelected };
+    },
+  },
   methods: {
     selectA() {
       this.boxASelected = !this.boxASelected;
