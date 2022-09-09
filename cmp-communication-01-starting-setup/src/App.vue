28e9256a4afee937d29a5404744c1d581cf462f8
Long Thay <long.tb@ht-tech.vn>
2022-09-09 16:54:08 +0700

    Add new friend to list

--- cmp-communication-01-starting-setup/src/App.vue
+++ cmp-communication-01-starting-setup/src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -45,6 +46,16 @@ export default {
       const foundFriend = this.friends.find((friend) => friend.id === id);
       foundFriend.isFavorite = !foundFriend.isFavorite;
     },
+    addContact(enteredName, enteredPhone, enteredEmail) {
+      const newFriend = {
+        id: new Date().toISOString(),
+        name: enteredName,
+        phone: enteredPhone,
+        email: enteredEmail,
+        isFavorite: false,
+      };
+      this.friends.push(newFriend);
+    },
   },
 };
 </script>
@@ -75,7 +86,8 @@ header {
   padding: 0;
   list-style: none;
 }
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -104,5 +116,18 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
