c6df2b9d73ceb3134d2a54a241f8e44c4c478a4c
Long Thay <long.tb@ht-tech.vn>
2022-09-09 09:49:18 +0700

    Understanding the lifecycle of Vue app

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -16,6 +16,42 @@ const app = Vue.createApp({
       this.message = this.$refs.userText.value;
     },
   },
+  beforeCreate() {
+    // Vue app is not fully initallized
+    // Could send http request, set timer
+    console.log("Before create");
+  },
+  created() {
+    console.log("Created");
+  },
+  beforeMount() {
+    // Right before we see something on the screen
+    console.log("Before mount");
+  },
+  mounted() {
+    // Templates and data are proceeded and showed on screen
+    console.log("Mounted");
+  },
+  beforeUpdate() {
+    // Right before we see change on the screen
+    console.log("Before update");
+  },
+  updated() {
+    // Data changes are proceeded and showed on the screen
+    console.log("Updated");
+  },
+  beforeUnmount() {
+    // Right before app.unmount called
+    console.log("Before unmount");
+  },
+  unmounted() {
+    // Successfully called app.unmount
+    console.log("Unmounted");
+  },
 });
 
 app.mount("#app");
+
+setTimeout(() => {
+  app.unmount("#app");
+}, 3000);
