6f0267acfeff5965371f5910b751e88ca32f13ce
Long Thay <thaybunlong20@yahoo.com>
2022-09-10 21:18:34 +0700

    Provide and Inject to pass data around

---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeBase.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h2>Select a Topic</h2>
+    <knowledge-grid
+      @select-topic="$emit('select-topic', $event)"
+    ></knowledge-grid>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ['select-topic'],
+};
+</script>
