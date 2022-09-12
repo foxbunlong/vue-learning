dd20aae5d2eb4a10cabe67591e030f9df8f177bf
Long Thay <long.tb@ht-tech.vn>
2022-09-12 11:36:36 +0700

    Apply slot for reused component

--- cmp-adv-01-starting-setup2/src/components/UserInfo.vue
+++ cmp-adv-01-starting-setup2/src/components/UserInfo.vue
@@ -1,29 +1,25 @@
 <template>
   <section>
-    <header>
-      <h3>{{ fullName }}</h3>
-      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </header>
-    <p>{{ infoText }}</p>
+    <base-card>
+      <header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </header>
+      <p>{{ infoText }}</p>
+    </base-card>
   </section>
 </template>
 
 <script>
+import BaseCard from "./BaseCard.vue";
 export default {
+  components: { BaseCard },
   props: ["fullName", "infoText", "role"],
 };
 </script>
 
 <style scoped>
 /* Scoped styles - Vue add generated attribute to selector */
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
 section header {
   display: flex;
   justify-content: space-between;
--- cmp-adv-01-starting-setup2/src/main.js
+++ cmp-adv-01-starting-setup2/src/main.js
@@ -2,12 +2,14 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import BaseBadge from "./components/BaseBadge.vue";
+import BaseCard from "./components/BaseCard.vue";
 
 const app = createApp(App);
 
 // Global components
 // Application has to download all components initially - Big problem for very big projects
 app.component("base-badge", BaseBadge); // Keep as global component due to usage
+app.component("base-card", BaseCard);
 // app.component("the-header", TheHeader);
 // app.component("badge-list", BadgeList);
 // app.component("user-info", UserInfo);
