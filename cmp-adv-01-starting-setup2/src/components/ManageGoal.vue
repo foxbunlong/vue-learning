673ff23cd1f73999bdbff2f991bd9d819930a6a8
Long Thay <long.tb@ht-tech.vn>
2022-09-14 15:36:59 +0700

    Teleport and understanding of Fragment in Vue 3

--- cmp-adv-01-starting-setup2/src/components/ManageGoal.vue
+++ cmp-adv-01-starting-setup2/src/components/ManageGoal.vue
@@ -1,13 +1,17 @@
 <template>
+  <!-- In Vue 2, it requires div to wrap all the components, but in Vue 3, we can remove the div tag - so called Fragment -->
   <div>
     <h2>Manage Goals</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">Set Goal</button>
-    <ErrorAlert v-if="!inputIsValid">
-      <h2>Input is invalid</h2>
-      <p>Please enter at least a few characters...</p>
-      <button @click="closeDialog">OK</button>
-    </ErrorAlert>
+    <!-- Built-in element to render component somewhere else in dom structure -->
+    <teleport to="body">
+      <ErrorAlert v-if="!inputIsValid">
+        <h2>Input is invalid</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="closeDialog">OK</button>
+      </ErrorAlert>
+    </teleport>
   </div>
 </template>
 
