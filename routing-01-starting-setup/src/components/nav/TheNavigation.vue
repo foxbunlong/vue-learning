5f6684af60dfd6e0547c02b4cc5773bd6e5be64b
Long Thay <long.tb@ht-tech.vn>
2022-09-20 15:41:11 +0700

    Apply navigation with router

--- routing-01-starting-setup/src/components/nav/TheNavigation.vue
+++ routing-01-starting-setup/src/components/nav/TheNavigation.vue
@@ -3,26 +3,17 @@
     <nav>
       <ul>
         <li>
-          <button @click="setActivePage('teams-list')">Teams</button>
+          <!-- <button @click="setActivePage('teams-list')">Teams</button> -->
+          <router-link to="/teams">Teams</router-link>
         </li>
         <li>
-          <button @click="setActivePage('users-list')">Users</button>
+          <!-- <button @click="setActivePage('users-list')">Users</button> -->
+          <router-link to="/users">Users</router-link>
         </li>
       </ul>
     </nav>
   </header>
 </template>
-
-<script>
-export default {
-  emits: ['set-page'],
-  methods: {
-    setActivePage(page) {
-      this.$emit('set-page', page);
-    },
-  },
-};
-</script>
 
 <style scoped>
 header {
@@ -49,7 +40,9 @@ li {
   margin: 0 2rem;
 }
 
-button {
+/* router-link uses a */
+a {
+  text-decoration: none;
   font: inherit;
   background: transparent;
   border: 1px solid transparent;
@@ -59,8 +52,8 @@ button {
   display: inline-block;
 }
 
-button:hover,
-button:active {
+a:hover,
+a:active {
   color: #f1a80a;
   border-color: #f1a80a;
   background-color: #1a037e;
