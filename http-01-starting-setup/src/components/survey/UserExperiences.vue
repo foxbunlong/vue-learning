35be07be68f29c57c9de0e4fbb944c442351419b
Long Thay <long.tb@ht-tech.vn>
2022-09-20 14:25:50 +0700

    Get data from firebase and show to screen

--- http-01-starting-setup/src/components/survey/UserExperiences.vue
+++ http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -3,7 +3,7 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="getData">Load Submitted Experiences</base-button>
       </div>
       <ul>
         <survey-result
@@ -24,6 +24,39 @@ export default {
   // props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    getData() {
+      fetch(
+        'https://vue-demo-46c9e-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json'
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          console.log(data);
+          const results = [];
+          // Loop through all objects in data
+          for (const id in data) {
+            results.push({
+              id,
+              name: data[id].userName,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        });
+    },
+  },
+  mounted() {
+    this.getData();
   },
 };
 </script>
