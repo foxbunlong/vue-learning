955c6c42fa94eba1f8b040a95cd2c7c7f80684dd
Long Thay <long.tb@ht-tech.vn>
2022-09-27 16:08:26 +0700

    Basic of VueX

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <TheCounter />
+    <button @click="add(1)">Add</button>
+    <ChangeCounter />
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+    ChangeCounter,
+  },
+  methods: {
+    add(num) {
+      this.$store.state.counter += num;
+    },
   },
 };
 </script>
---
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="add(1)">Add</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    add(num) {
+      this.$store.state.counter += num;
+    },
+  },
+};
+</script>
