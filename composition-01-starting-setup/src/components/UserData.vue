c404f46e173f78f88ec3e416e465695039c3ceb4
Long Thay <long.tb@ht-tech.vn>
2022-12-08 14:18:03 +0700

    Composition api - Lifecycle Hook

--- composition-01-starting-setup/src/components/UserData.vue
+++ composition-01-starting-setup/src/components/UserData.vue
@@ -9,9 +9,43 @@
 </template>
 
 <script setup>
-import { computed, defineProps, inject } from 'vue';
+import {
+  computed,
+  defineProps,
+  inject,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue';
 
 const age = inject('userAge');
+
+onBeforeMount(() => {
+  console.log('onBeforeMount');
+});
+
+onMounted(() => {
+  console.log('onMounted');
+});
+
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate');
+});
+
+onUpdated(() => {
+  console.log('onUpdated');
+});
+
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount');
+});
+
+onUnmounted(() => {
+  console.log('onUnmounted');
+});
 
 const props = defineProps({
   firstName: {
